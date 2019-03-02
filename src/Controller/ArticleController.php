b6d2d101da90159bbf119e28a26bc2699b1fce9b
David Ceranic <david.ceranic@gmail.com>
2019-03-02 10:35:32 +0100

    Add simple bootstrap navigation & twig template engine

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,15 +6,19 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 //to limit http requests
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ArticleController {
+
+class ArticleController extends Controller {
     /**
      * @Route("/")
      * @Method({"GET"})
      */
 
     public function index() {
-        return new Response('<html><body>Hello</body></html>');
+        // return new Response('<html><body>Hello</body></html>');
+
+        return $this->render('articles/index.html.twig');
     }
 
 }
