5a36b759dff97bfef6cd16ca126362ec0ea66fdc
David Ceranic <david.ceranic@gmail.com>
2019-03-03 11:15:59 +0100

    Add form packet &  method for saving articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,12 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 //to limit http requests
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+//to use form
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+//for requests & responses
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends Controller {
     /**
@@ -22,15 +29,6 @@ class ArticleController extends Controller {
         $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render('articles/index.html.twig', array('articles' => $articles));
-    }
-
-    /**
-     * @Route("/article/{id}", name="article_show")
-     */
-    public function show($id) {
-        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
-
-        return $this->render('articles/show.html.twig', array('article' => $article));
     }
 
     // /**
@@ -48,5 +46,43 @@ class ArticleController extends Controller {
 
     //     return new Response('Saved an article with an id = '. $article->getId());
     // }
+
+    /**
+     * @Route("/article/new", name="new_article")
+     * Method({"GET","POST"})
+     */
+
+     public function new(Request $request) {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                      ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+                      ->add('body', TextAreaType::class, array('required' => false, 'attr' => array('class' => 'form-control')))
+                      ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
+                      ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', array('form' => $form->createView()));
+     }
+
+     /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show($id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/show.html.twig', array('article' => $article));
+    }
 
 }
